1f2c7939850860acf04a125b5554a281bee54108
Kris Kaleta <72305222+dziki610@users.noreply.github.com>
2020-11-05 11:18:57 +0000

    Create DogYears.js
    CA Project: Dog Years

---
+++ Codecademy/JavaScriptIntro/DogYears.js
@@ -0,0 +1,10 @@
+let myAge = 31;
+let earlyYears = 2;
+earlyYears*=10.5;
+laterYears = myAge - 2;
+laterYears*=4;
+console.log(earlyYears);
+console.log(laterYears);
+myAgeInDogYears = earlyYears + laterYears;
+myName = "Kris".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
